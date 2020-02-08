55ce69862d0f0d15ed79d4edcd45a4f350d13b33
Vidhya Sagar Thakur <60700534+vsthakur101@users.noreply.github.com>
2020-02-09 00:40:30 +0530

    Update Api_ref.js

--- src/api/Api_ref.js
+++ src/api/Api_ref.js
@@ -1,2 +1,2 @@
-export const heroku_url="https://apidevcommunity.herokuapp.com/";
+export const heroku_url="https://clever-hodgkin-11cbed.netlify.com/";
 export const local_host="http://localhost:5000"
